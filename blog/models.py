4dc46c4d6f1c7d7ff0e045631b5581e50eb28435
Me <me@example.com>
2019-07-08 01:00:53 +0300

    Добавление идентификации пользователей

--- blog/models.py
+++ blog/models.py
@@ -6,11 +6,15 @@ class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200, verbose_name='Заголовок')
     text = models.TextField(verbose_name='Текст статьи')
-    created_date = models.DateTimeField(default=timezone.now)
-    published_date = models.DateTimeField(blank=True, null=True)
+    created_date = models.DateTimeField(default=timezone.now, verbose_name='Дата создания')
+    published_date = models.DateTimeField(blank=True, null=True, verbose_name='Дата публикации')
 
     def publish(self):
         self.published_date = timezone.now()
+        self.save()
+
+    def unpublish(self):
+        self.published_date = None
         self.save()
 
     def __str__(self):
